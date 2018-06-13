c554328dd930311d3504a90b9016c803302cc0d9
Nick <nicholasjohnrichardson@gmail.com>
2018-06-13 15:05:58 +0100

    planes can take off from an airport

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -16,5 +16,10 @@ describe('Airport', function(){
     airport.clearForLanding(plane);
     expect(airport.planes()).toEqual([plane]);
   });
-  
+
+  it('can clear plans for takeoff', function(){
+    airport.clearForLanding(plane);
+    airport.clearForTakeOff(plane);
+    expect(airport.planes()).toEqual([]);
+  });
 });
